c7e236f452675a39b94b6338be97c229500f8d89
Giovana Luciana De Campos Costa <giovanaluciana77@gmail.com>
2024-09-13 07:57:41 -0300

    2

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const avanca = document.querySelectorAll(' .btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const
+        const proximoPasso = 'passo-' + this.getAttrbute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
